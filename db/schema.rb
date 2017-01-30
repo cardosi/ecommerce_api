f5a3c130665de854be48b7efa9ba8d03d17cab55
CCAZ88 <camcardosi@mac.com>
2017-01-30 09:08:52 -0700

    basic jwt working on backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129005302) do
+ActiveRecord::Schema.define(version: 20170130003259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,15 +38,15 @@ ActiveRecord::Schema.define(version: 20170129005302) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "email"
     t.string   "name"
     t.string   "address"
     t.string   "city"
     t.string   "state"
     t.integer  "zip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "transactions", "products"
